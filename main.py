9de709a58816bd9eef92f4dc77b8220c45b44693
kassyiemobi <omeh.kasie@gmail.com>
2020-03-06 20:54:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# to check the lenght of a password in the case of a forgotten password
+username= input("what is your username")
+password= input("what is your password")
+
+password_lenght = len(password)
+hidden_password = "*" * password_lenght
+print(f'{username} your password {hidden_password} is {password_lenght} letters long')
